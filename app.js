4e55e068aabad10d03acb3211dd6cc0cd69446e8
hesham <heshamabdulaziz88@gmail.com>
2026-03-26 05:16:02 -0700

    handle  status code

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const http=require("http")
 const server=http.createServer((req,res)=>{
 // create server
 res.setHeader("Content-Type","text/html")
+
+res.statusCode=200
+// 200 ok 
+//404  not found
 const header=req.headers;
 res.write("<h2> hi from server</h2>");
 res.write("<h2>how i can help you\n");
-res.write(`${header}`);
+
 res.end()
 
 })
