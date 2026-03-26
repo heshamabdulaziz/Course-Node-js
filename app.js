d0245d27ef5210e2d2057522733850218ee640bb
hesham <heshamabdulaziz88@gmail.com>
2026-03-26 04:34:56 -0700

    create server with http

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const app=require("express");
+const { log } = require("console")
+const http=require("http")
+// this built in module use to creat server 
+// server is program install in big system or computer
 
-const vars=require("./modules");
+const server=http.createServer((req,res)=>{
+// create server
+res.write("<h2> hi from server");
+res.write("how i can help you");
+res.end()
 
-console.log(vars);
-console.log(vars.sum(10,40));
-console.log(vars.multi(10,40));
+})
+const port=4000;
+// run server
+server.listen(port,()=>{
 
+    console.log(`SErver run on port ${port} `);
+    
+})
