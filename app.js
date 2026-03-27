0cb1681adb1a301658153e469b1c44b52b1c5fca
hesham <heshamabdulaziz88@gmail.com>
2026-03-26 22:53:35 -0700

    to run any middleware we use app.use(middleware 2- middleware run from up to down we use third parmater  call next and call it in then end any middlware to moveto next middleware

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 // Start with Express
 const express=require("express");
+const { log } = require("node:console");
 const app=express()
+// express buit by middlewar 
+//middleware is funation excute with each  comming request from client
+
+app.use((req,res,next)=>{
+
+   // res.send("<h1>hello from m1</h1>")
+       console.log("hello from m1");
+       next()
+     
+})
+app.use((req,res)=>{
+   console.log("hello from m2");
+    res.send("<h1>hello from m2</h1>")
+    
+})
 
 const port=4000;
 // run server
