87a8a6331cbea6347a656671aad4e36afd640913
hesham <heshamabdulaziz88@gmail.com>
2026-03-26 22:08:25 -0700

    Create server  with express middlewar

--- app.js
+++ app.js
@@ -1,35 +1,11 @@
-const { log } = require("console")
-const http=require("http")
-// this built in module use to creat server 
-// server is program install in big system or computer
+// Start with Express
+const express=require("express");
+const app=express()
 
-const server=http.createServer((req,res)=>{
-// create server
-res.setHeader("Content-Type","text/html")
-
-res.statusCode=200
-// 200 ok 
-//404  not found
-//400 eeror
-//500
-// req.url return endpoint
-//req.method return method( get or post or put or delete)
-if(req.url==="/"){
-res.write(`<h2> Home page  AND Metod :  ${req.method}</h2>`);}
-
-if(req.url==="/about"){
-
-// send()  same write() but no need to add end() or handle the header content
-//setHeader("Content-Type","text/html")
-res.send(`<h2> About page  AND Metod :  ${req.method}</h2>`);}
-
-
-
-})
 const port=4000;
 // run server
-server.listen(port,()=>{
+app.listen(port,()=>{
 
-    console.log(`SErver run on port ${port} `);
+    console.log(`Server run on port ${port}  with express `);
     
 })
