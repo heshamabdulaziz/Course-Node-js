12c6a5afe342ab74cdb981927b70a994bb02bb10
hesham <heshamabdulaziz88@gmail.com>
2026-03-27 01:22:46 -0700

    add use() with routig

--- app.js
+++ app.js
@@ -5,21 +5,19 @@ const app=express()
 // express buit by middlewar 
 //middleware is funation excute with each  comming request from client
 
-app.use((req,res,next)=>{
+app.use("/",(req,res,next)=>{
        console.log("hello from m1");
-       next()//go to next m2
-     
-  },(req,res,next)=>{
+      res.send("<h1>hello from m1</h1>")
+     })
+app.use("/aboute",(req,res)=>{
    console.log("hello from m2");
-    //res.send("<h1>hello from m2</h1>")
-   next()//go to next m3
-}
-)
-app.use((req,res)=>{
-   console.log("hello from m3");
-    res.send("<h1>hello from m3</h1>")
-    
-})
+    res.send("<h1>hello from m2</h1>")
+    })
+    // if routing error return 404
+    app.use((req,res)=>{
+   
+    res.send("<h1>page not found 404</h1>")
+    })
 
 const port=4000;
 // run server
