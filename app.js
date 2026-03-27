ebe02eb28599742f2403af14a951923d08681108
hesham <heshamabdulaziz88@gmail.com>
2026-03-26 23:04:54 -0700

    app.use can take more than function and we use next to move between funcatins in exampe we move from m1 to m2 than m3

--- app.js
+++ app.js
@@ -6,15 +6,18 @@ const app=express()
 //middleware is funation excute with each  comming request from client
 
 app.use((req,res,next)=>{
-
-   // res.send("<h1>hello from m1</h1>")
        console.log("hello from m1");
-       next()
+       next()//go to next m2
      
-})
-app.use((req,res)=>{
+  },(req,res,next)=>{
    console.log("hello from m2");
-    res.send("<h1>hello from m2</h1>")
+    //res.send("<h1>hello from m2</h1>")
+   next()//go to next m3
+}
+)
+app.use((req,res)=>{
+   console.log("hello from m3");
+    res.send("<h1>hello from m3</h1>")
     
 })
 
