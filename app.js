19ff7d629f7409fe056352d741532ffa940d95bf
hesham <heshamabdulaziz88@gmail.com>
2026-03-28 08:54:19 -0700

    add ejs and use render() to retun  esj to browser instead .html

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express=require("express");
 const path=require("path")
 const app=express()
+// settigng   Template Engines ejs
+app.set("view engine","ejs")//ejs engine
+app.set("views","view")// path of my files
 app.use(express.static(path.join(__dirname,'statics')))
 // to access to serve files like css or js we use express.static() middleware
 const aboutRouter=require("./routing/aboutRouter")
@@ -10,7 +13,7 @@ const bodyparser=require("body-parser")
 const bodypars=bodyparser.urlencoded({extended:true})
 app.get("/",(req,res)=>{
 
-res.sendFile(path.join(__dirname,'view','index.html')) 
+res.render('index')
      })
     
 app.post('/',bodypars,(req,res)=>{
