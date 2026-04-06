83e371f006418dcd9efd7773f82e6af733aca615
hesham <heshamabdulaziz88@gmail.com>
2026-04-06 08:14:32 -0700

    Add exports&require

---
+++ exports_Modules.js
@@ -0,0 +1,10 @@
+// first way 
+
+const lastName="Abdulaziz";
+
+//second way 
+
+module.exports={
+lastName,
+}
+module.exports.firstName="hesham ";
---
+++ globals.js
@@ -0,0 +1,14 @@
+// __direname  
+// this return file folder
+console.log(__dirname);
+
+// return file name
+console.log(__filename);
+
+// module.exports
+const names=require("./exports_Modules")
+
+console.log("FirstName LastName");
+ console.log(names.firstName, names.lastName);
+ 
+ 
