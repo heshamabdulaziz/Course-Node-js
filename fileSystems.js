e8586b00b87c551b5933d0d4668b4bfca32449f0
hesham <heshamabdulaziz88@gmail.com>
2026-04-07 02:33:51 -0700

    wwrite and read content from files

--- fileSystems.js
+++ fileSystems.js
@@ -4,3 +4,8 @@ const file_1=readFileSync("./test1.txt","utf-8");
 const  file_2=readFileSync("./test2.txt","utf-8");
 
 console.log(file_1,"\n",file_2)
+// write to files 
+
+const writee=writeFileSync("./test3.txt",file_1)
+console.log("%%%%%%  Read the Content from test3  %%%%%%%%%")
+console.log(readFileSync("./test3.txt","utf-8"))
